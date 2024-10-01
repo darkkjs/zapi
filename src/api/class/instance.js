20abc00200268c777bd6e705dd9dd75a2e32ecba
darkkjs <higorolive2025@gmail.com>
2024-09-30 22:32:59 -0300

    att

--- src/api/class/instance.js
+++ src/api/class/instance.js
@@ -824,7 +824,8 @@ console.log("confirmada")
 				
 			}
 
-				await this.SendWebhook('message', 'messages.upsert', webhookData, this.key);
+				const webh = await this.SendWebhook('message', 'messages.upsert', webhookData, this.key);
+                console.log("webhook enviado", webh);
             }
 			catch(e)
 			{
